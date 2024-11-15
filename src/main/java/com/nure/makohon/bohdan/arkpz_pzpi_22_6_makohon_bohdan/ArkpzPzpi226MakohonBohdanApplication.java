f3eb1568d73f47ac9016a77e5bb525ea7fb031f2
NureMakohonBohdan <bohdan.makohon@nure.ua>
2024-11-15 16:53:41 +0000

    start of project

---
+++ src/main/java/com/nure/makohon/bohdan/arkpz_pzpi_22_6_makohon_bohdan/ArkpzPzpi226MakohonBohdanApplication.java
@@ -0,0 +1,13 @@
+package com.nure.makohon.bohdan.arkpz_pzpi_22_6_makohon_bohdan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArkpzPzpi226MakohonBohdanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArkpzPzpi226MakohonBohdanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nure/makohon/bohdan/arkpz_pzpi_22_6_makohon_bohdan/ArkpzPzpi226MakohonBohdanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nure.makohon.bohdan.arkpz_pzpi_22_6_makohon_bohdan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArkpzPzpi226MakohonBohdanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
